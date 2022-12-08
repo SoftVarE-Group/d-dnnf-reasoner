ea956e73954a1142972a604495190f093789505e
Lars Licha <lars.licha@uni-ulm.de>
2022-12-08 16:34:58 +0100

    add lexicographic pair iterator

--- src/sampler/iterator.rs
+++ src/sampler/iterator.rs
@@ -1,16 +1,23 @@
+/// Creates an iterator for pairs (t=2) of features.
+/// The iteration order is lexicographic.
+pub fn pair_iter(number_of_vars: i32) -> impl Iterator<Item=Vec<i32>> {
+    internal_pair_iter(number_of_vars).map(|(i, j)| vec![i, j])
+}
+
 /// Creates an iterator for pair wise (t=2) interactions.
 /// The iteration order is lexicographic.
-fn lexicographic_pair_wise_iter(
+pub fn pair_wise_interaction_iter(
     number_of_vars: i32,
 ) -> impl Iterator<Item=Vec<i32>> {
-    (1..=number_of_vars)
-        .into_iter()
-        .flat_map(move |i| {
-            (i + 1..=number_of_vars).into_iter().map(move |j| (i, j))
-        })
-        .flat_map(|(i, j)| {
-            vec![vec![i, j], vec![i, -j], vec![-i, j], vec![-i, -j]].into_iter()
-        })
+    internal_pair_iter(number_of_vars).flat_map(|(i, j)| {
+        vec![vec![i, j], vec![i, -j], vec![-i, j], vec![-i, -j]].into_iter()
+    })
+}
+
+fn internal_pair_iter(number_of_vars: i32) -> impl Iterator<Item=(i32, i32)> {
+    (1..=number_of_vars).into_iter().flat_map(move |i| {
+        (i + 1..=number_of_vars).into_iter().map(move |j| (i, j))
+    })
 }
 
 #[cfg(test)]
@@ -18,8 +25,17 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_pair_wise_lexicographic() {
-        let mut iter = lexicographic_pair_wise_iter(3);
+    fn test_pair_iter() {
+        let mut iter = pair_iter(3);
+
+        assert_eq!(Some(vec![1, 2]), iter.next());
+        assert_eq!(Some(vec![1, 3]), iter.next());
+        assert_eq!(Some(vec![2, 3]), iter.next());
+    }
+
+    #[test]
+    fn test_pair_interaction_iter() {
+        let mut iter = pair_wise_interaction_iter(3);
 
         assert_eq!(Some(vec![1, 2]), iter.next());
         assert_eq!(Some(vec![1, -2]), iter.next());
