3744c9b20ba34dc662cbf1da7b2c134dded0f6c2
Jan Baudisch <jan.baudisch@uni-ulm.de>
2025-01-26 19:32:50 +0100

    feat(dependencies): update

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    crane.url = "github:ipetkov/crane/v0.19.3";
+    crane.url = "github:ipetkov/crane/v0.20.0";
     d4 = {
       url = "github:SoftVarE-Group/d4v2/2.1.1";
       inputs.nixpkgs.follows = "nixpkgs";
